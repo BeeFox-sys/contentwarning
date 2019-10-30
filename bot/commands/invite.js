2c4dfc4f10e584a9276afa0581754ffa89f16c0d
Fiona <fiona.greenwood.sheridan@gmail.com>
2019-10-30 12:57:26 +1100

    Invite.js

---
+++ bot/commands/invite.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const utils = require('../utils.js')
+
+module.exports = {
+	name: 'about',
+	aliases: ['invite'],
+	description: 'Responds to the user',
+    hidden: false,
+	perms: null,
+	guild: false,
+	async execute(client, msg, args) {
+		return await msg.channel.send(client.config.about)
+	},
+};
